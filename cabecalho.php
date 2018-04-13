679de5736c81fd3fa3533661b17cb5835233224b
Andretoscano123 <andrearanha123@hotmail.com>
2018-04-13 08:08:28 -0300

    Limpeza e Revisão pré-css. Inclusão do cabecalho e remoção de arquivos pendentes

---
+++ cabecalho.php
@@ -0,0 +1,14 @@
+<!-- André Calixto Toscano -->
+<div id="cabecalho">
+	<header>
+		<h1>Trabalho de WEB - Bimestre 1</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="form_estado.php">Casdastrar Estado</a></li>
+			<li><a href="form_cidade.php">Cadastrar Cidade</a></li>
+			<li><a href="form_cadastro.php">Cadastrar Pessoa</a></li>
+		</ul>
+	</nav>
+</div>
